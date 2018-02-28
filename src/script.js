cca56148f15dc3a7c0cd3d62523b72f10c635d16
Vitalii Kalaida <kalaidavitalii@gmail.com>
2018-02-28 23:37:53 +0200

    ES2015: Spread operator testing

--- src/script.js
+++ src/script.js
@@ -1,9 +1,9 @@
-let name = 'hello es6';
+// let name = 'hello es6';
 
-if (true) {
-	let checker = 'es6 scope check';
-}
-console.log(checker);
+// if (true) {
+// 	let checker = 'es6 scope check';
+// }
+// console.log(checker);
 
 // Part 2:
 // Let vs Var checking
@@ -26,3 +26,36 @@ function SelecorCache()
 var selector = new SelecorCache();
 
 var buttons = selector.get('button');
+
+// for (var i = 0; i < buttons.length; i++) { // ONCLICK THE SAME RESULT FOR EVERY BUTTON	
+for (let i = 0; i < buttons.length; i++) {
+	var button = buttons[i];
+	button.innerText = i;
+	button.onclick = function() {
+		console.log(i);
+	}
+}
+
+// PART 3: Consts
+
+const LIGHT_SPEED = 3000000; // Will not reassign after update 
+
+
+// PART 4: Spread operator
+
+let staticLanguages = ['C', 'C++', 'C#'];
+let dynamicLanguages = ['Javascript', 'PHP', 'Ruby'];
+let languages = [...staticLanguages, 'C#', ...dynamicLanguages, 'Python'];
+
+console.log([...staticLanguages]); // Spread and back it again (nothing changes)
+
+console.log(languages);
+
+function add(x, y, z)
+{
+	console.log(x + y + z);
+}
+
+let numbers = [1, 2, 3];
+
+add(...numbers); // Spreading array into numbers (SIMILAR TO PHP)
