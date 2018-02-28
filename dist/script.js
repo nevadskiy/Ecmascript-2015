053b7b3095aeb3a2eec46c21fdae86b312a48c3b
Vitalii Kalaida <kalaidavitalii@gmail.com>
2018-02-28 23:07:15 +0200

    ES2015 babel presets configurated

---
+++ dist/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var name = 'hello es6';
+
+if (true) {
+	var _checker = 'es6 scope check';
+}
+
+console.log(checker);
---
+++ src/script.js
@@ -0,0 +1,7 @@
+let name = 'hello es6';
+
+if (true) {
+	let checker = 'es6 scope check';
+}
+
+console.log(checker);
