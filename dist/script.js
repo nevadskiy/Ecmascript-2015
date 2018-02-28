dd694cd06a6a28c20a6e8b574a7d9545675f30ca
Vitalii Kalaida <kalaidavitalii@gmail.com>
2018-02-28 23:16:33 +0200

    Feature caching DOM selectors

--- dist/script.js
+++ dist/script.js
@@ -5,5 +5,24 @@ var name = 'hello es6';
 if (true) {
 	var _checker = 'es6 scope check';
 }
-
 console.log(checker);
+
+// Part 2:
+// Let vs Var checking
+
+// SELECTOR CACHING METHOD
+function SelecorCache() {
+	var collection = {};
+
+	function getFromCache(selector) {
+		if (undefined === collection[selector]) {
+			collection[selector] = document.querySelectorAll(selector);
+		}
+		return collection[selector];
+	}
+
+	return { get: getFromCache };
+}
+var selector = new SelecorCache();
+
+var buttons = selector.get('button');
--- src/script.js
+++ src/script.js
@@ -3,5 +3,26 @@ let name = 'hello es6';
 if (true) {
 	let checker = 'es6 scope check';
 }
-
 console.log(checker);
+
+// Part 2:
+// Let vs Var checking
+
+// SELECTOR CACHING METHOD
+function SelecorCache()
+{
+	var collection = {};
+
+	function getFromCache(selector)
+	{
+		if (undefined === collection[selector]) {
+			collection[selector] = document.querySelectorAll(selector);
+		}
+		return collection[selector];
+	}
+
+	return { get: getFromCache };
+}
+var selector = new SelecorCache();
+
+var buttons = selector.get('button');
