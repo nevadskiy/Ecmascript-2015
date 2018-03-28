27a8fc7ef05f63e13fd09266f946536170ff65ac
Nevadskiy <nevadskiy@gmail.com>
2018-03-29 00:02:43 +0300

    Promises scipts added

--- dist/promises.js
+++ dist/promises.js
@@ -1,13 +1,14 @@
 'use strict';
 
+/**
 // PROMISES SUMILATION
 // Register function with callbacks
 // then callback it
 function applyForVisa(documents, resolve, reject) {
     console.log('Process applying');
 
-    setTimeout(function () {
-        var visa = 12;
+    setTimeout(function() {
+        let visa = 12;
         Math.random() > .5 ? resolve(12) : reject('just denied');
         // resolve(visa);
         // resolve(visa);
@@ -19,32 +20,87 @@ function applyForVisa(documents, resolve, reject) {
     }, 2000);
 }
 
-function bookHotel(visaId, resolve, reject) {}
 
-function buyTicket() {}
+function bookHotel(visaId, resolve, reject) {
+    
+}
+
+function buyTicket() {
+    // the same callback shits
+}
 
 //calling function with 2 callbacks anonymouses
-applyForVisa({}, function (id) {
-    console.info('Visa ' + id + ' is received');
+applyForVisa({}, function(id) {
+    console.info('Visa '+ id +' is received');
 
     //calling again 2 anonymouses callbacks
-    bookHotel(id, function (reservation) {
+    bookHotel(id, function(reservation) {
         // resolving promise
         console.log(reservation);
-        buyTicket(reservation, function () {
+        buyTicket(reservation, function() {
             //resolving
-        }, function () {
+        }, function() {
             //rejecting
 
             // THATS REAL CALLBACK HELL
             // THATS REAL CALLBACK HELL
             // THATS REAL CALLBACK HELL
-        });
-    }, function (fail) {
+        })
+    }, function(fail) {
         // rejecting promise
         console.error(fail);
-    });
-}, function (reason) {
+    })
+
+}, function(reason) {
     // rejecting first promise
     console.error(reason);
 });
+*/
+
+// PROMISES HERE
+function applyForVisa(documents) {
+    console.log('Process applying');
+
+    var promise = new Promise(function (res, rej) {
+        setTimeout(function () {
+            Math.random() > .5 ? res(12) : rej('just denied');
+        }, 2000);
+    });
+
+    return promise;
+}
+
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Hotel is booked');
+    return { book: 'mac' };
+}
+
+function buyTicket(book) {
+    console.log('tickets is bought');
+    console.log(book);
+}
+
+function getVisa(visa) {
+    console.log(visa + ' is accepted');
+    // need to return visa so next .then chain can use it in his body
+    return visa;
+}
+
+applyForVisa({})
+// multiple then in success case
+.then(getVisa)
+// new promise was created here and returns automatically
+// we can use it and can not
+.then(bookHotel)
+// arg is that function above returns
+// to assign two or more args we should return new promise in function above
+// with method reject then accept two or more args OR with object Promise.resolve that
+// that instantly do resolve (also can instant reject)
+.then(buyTicket)
+// reject is called
+.catch(function (error) {
+    return console.log(error);
+});
+
+// Promise can be in 3 statuses: pending, resolved, rejected
--- src/promises.js
+++ src/promises.js
@@ -1,3 +1,4 @@
+/**
 // PROMISES SUMILATION
 // Register function with callbacks
 // then callback it
@@ -23,7 +24,7 @@ function bookHotel(visaId, resolve, reject) {
 }
 
 function buyTicket() {
-
+    // the same callback shits
 }
 
 //calling function with 2 callbacks anonymouses
@@ -52,3 +53,51 @@ applyForVisa({}, function(id) {
     // rejecting first promise
     console.error(reason);
 });
+*/
+
+
+// PROMISES HERE
+function applyForVisa(documents) {
+    console.log('Process applying');
+
+    let promise = new Promise(function(res, rej) {
+        setTimeout(function() {
+            Math.random() > .5 ? res(12) : rej('just denied');
+        }, 2000);
+    });
+
+    return promise;
+}
+
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Hotel is booked');
+    return {book: 'mac'}
+}
+
+function buyTicket(book) {
+    console.log('tickets is bought')
+    console.log(book);
+}
+
+function getVisa(visa) {
+    console.log(visa + ' is accepted');
+    // need to return visa so next .then chain can use it in his body
+    return visa;
+}
+
+applyForVisa({})
+    // multiple then in success case
+    .then(getVisa)
+    // new promise was created here and returns automatically
+    // we can use it and can not
+    .then(bookHotel)
+    // arg is that function above returns
+    // to assign two or more args we should return new promise in function above
+    // with method reject then accept two or more args OR with object Promise.resolve that
+    // that instantly do resolve (also can instant reject)
+    .then(buyTicket)
+    // reject is called
+    .catch(error => console.log(error));
+
+// Promise can be in 3 statuses: pending, resolved, rejected
